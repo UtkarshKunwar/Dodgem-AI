30fdacae9c54596a771e992e392f09616887e68c
DeathStocker <utkarsh2602@gmail.com>
2017-10-14 13:21:51 +0530

    Added alphabeta with random eval.

--- framework/Player.py
+++ framework/Player.py
@@ -13,11 +13,42 @@ class Player:
     def __init__(self):
         pass
 
-    def get_move(self,board):
+    def get_move(self, board):
         '''
         Override this method to create your player
         :param board: Board object
         :return: move i the format (piece,new_position,BoardLeavingMove)
         '''
-        moves = board.get_valid_moves()
-        return random.sample(moves, 1)[0]
+
+        return alphabeta(board)
+
+    def eval(self, board):
+        return random.uniform(-1000, 1000)
+
+    def alphabeta(self, board, max_player=True, alpha=float('-inf'), beta=float('inf'), max_ply=3):
+        if max_ply == 1:
+            return eval(board)
+
+        board_copy=[row[:] for row in board]
+        if max_player:
+            best_val = float('-inf')
+            for move in board.get_valid_moves():
+                board_copy.make_move(move)
+                val = alphabeta(board_copy, max_player=false, alpha=alpha, beta=beta, max_ply=max_ply - 1)
+                best_val = max(best_val, val)
+                alpha = max(alpha, best_val)
+
+                if alpha >= beta:
+                    break
+            return best_val
+        else:
+            best_val = float('inf')
+            for move in board.get_valid_moves():
+                board_copy.make_move(move)
+                val = alphabeta(board_copy, max_player=True, alpha=alpha, beta=beta, max_ply=max_ply - 1)
+                best_val = min(best_val, val)
+                beta = min(beta, best_val)
+
+                if alpha >= beta:
+                    break
+            return best_val
