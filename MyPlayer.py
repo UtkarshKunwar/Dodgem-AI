51f6f6ebcaffe763b44c33be65c57a416fa305bf
DeathStocker <utkarsh2602@gmail.com>
2017-10-18 01:26:39 +0530

    Added stuff from Player.py
    Need to refine alphabeta.

--- MyPlayer.py
+++ MyPlayer.py
@@ -1,22 +1,57 @@
 '''
-    File name: MyPlayer.py
+    File name: Player.py
     Author: Siddhant Kumar
     Email: saytosid@gmail.com
     Date created: 1 Oct 2017
     Date last modified: 1 Oct 2017
     Python Version: 3.0
 '''
-from framework import Player
+import random, copy
 
-class MyPlayer(Player):
-
+class MyPlayer:
+    '''Defines a Random Move Player'''
     def __init__(self):
         pass
 
-    def get_move(self,board):
+    def get_move(self, board):
         '''
+        Override this method to create your player
         :param board: Board object
-        :return: (piece,new_position,BoardLeavingMove)
+        :return: move i the format (piece,new_position,BoardLeavingMove)
         '''
-        moves = board.get_valid_moves()
-        return moves[0]
+
+        return self.alphabeta(board)
+
+    def eval(self, board):
+        return random.uniform(-1000, 1000)
+
+    def alphabeta(self, board, max_player=True, alpha=float('-inf'), beta=float('inf'), max_ply=3):
+        if max_ply == 1:
+            return self.eval(board)
+
+        board_copy = copy.deepcopy(board)
+        if max_player is True:
+            best_val = float('-inf')
+            best_move = ()
+            for move in board.get_valid_moves():
+                board_copy.make_move(move)
+                val = self.alphabeta(board_copy, max_player=False, alpha=alpha, beta=beta, max_ply=max_ply - 1)
+                best_val = max(best_val, val)
+                alpha = max(alpha, best_val)
+                best_move = move
+
+                if alpha >= beta:
+                    break
+            return best_move
+        else:
+            best_val = float('inf')
+            best_move = ()
+            for move in board.get_valid_moves():
+                board_copy.make_move(move)
+                val = self.alphabeta(board_copy, max_player=True, alpha=alpha, beta=beta, max_ply=max_ply - 1)
+                best_val = min(best_val, val)
+                beta = min(beta, best_val)
+
+                if alpha >= beta:
+                    break
+            return best_move
--- framework/Player.py
+++ framework/Player.py
@@ -6,7 +6,7 @@
     Date last modified: 1 Oct 2017
     Python Version: 3.0
 '''
-import random
+import random, copy
 
 class Player:
     '''Defines a Random Move Player'''
@@ -20,35 +20,38 @@ class Player:
         :return: move i the format (piece,new_position,BoardLeavingMove)
         '''
 
-        return alphabeta(board)
+        return self.alphabeta(board)
 
     def eval(self, board):
         return random.uniform(-1000, 1000)
 
     def alphabeta(self, board, max_player=True, alpha=float('-inf'), beta=float('inf'), max_ply=3):
         if max_ply == 1:
-            return eval(board)
+            return self.eval(board)
 
-        board_copy=[row[:] for row in board]
-        if max_player:
+        board_copy = copy.deepcopy(board)
+        if max_player is True:
             best_val = float('-inf')
+            best_move = ()
             for move in board.get_valid_moves():
                 board_copy.make_move(move)
-                val = alphabeta(board_copy, max_player=false, alpha=alpha, beta=beta, max_ply=max_ply - 1)
+                val = self.alphabeta(board_copy, max_player=False, alpha=alpha, beta=beta, max_ply=max_ply - 1)
                 best_val = max(best_val, val)
                 alpha = max(alpha, best_val)
+                best_move = move
 
                 if alpha >= beta:
                     break
-            return best_val
+            return best_move
         else:
             best_val = float('inf')
+            best_move = ()
             for move in board.get_valid_moves():
                 board_copy.make_move(move)
-                val = alphabeta(board_copy, max_player=True, alpha=alpha, beta=beta, max_ply=max_ply - 1)
+                val = self.alphabeta(board_copy, max_player=True, alpha=alpha, beta=beta, max_ply=max_ply - 1)
                 best_val = min(best_val, val)
                 beta = min(beta, best_val)
 
                 if alpha >= beta:
                     break
-            return best_val
+            return best_move
